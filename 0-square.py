4b0a740ef5b0dd65d61450218a7a82d84e49509c
hilal006 <hilalquliyeff@gmail.com>
2026-04-23 13:44:54 +0000

    smaksn

--- 0-square.py
+++ 0-square.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""
-This module provides a simple Square class.
-The module is used to demonstrate the basics of OOP in Python.
-"""
+"""Module that defines a Square class"""
 
 
 class Square:
-    """
-    A class used to represent a Square.
-    Currently, this class is empty and has no attributes or methods.
-    """
+    """Empty class Square"""
     pass
