daa382f969ea8ba89bf701540226417ebb40b60a
hilal006 <hilalquliyeff@gmail.com>
2026-04-23 12:30:36 +0000

    sasaa

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for text indentation function.
+"""
+
+
 def text_indentation(text):
     """
-    Prints text with 2 new lines after ., ? and :
+    Prints a text with 2 new lines after each of these characters: ., ? and :
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # This flag tracks if we are at the start of a new line
-    # to help us skip leading spaces.
     skip_space = True
-
     for char in text:
         if skip_space and char == " ":
             continue
-        
-        # Once we hit a non-space character, stop skipping
+
         skip_space = False
-        
         print(char, end="")
 
-        # If we hit a special character, print newlines and 
-        # set flag to skip the next spaces
         if char in ".?:":
             print("\n")
             skip_space = True
