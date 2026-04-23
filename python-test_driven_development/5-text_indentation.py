5a0cb34c036a9a57021095d4948653b129a8f92a
hilal006 <hilalquliyeff@gmail.com>
2026-04-23 12:24:26 +0000

    Sasa

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,15 +12,16 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    while i < len(text):
-        print(text[i], end="")
+    new_line = True
 
-        if text[i] in ".?:":
-            print("\n")
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
+    for char in text:
+        if new_line and char == " ":
             continue
 
-        i += 1
+        print(char, end="")
+
+        if char in ".?:":
+            print("\n")
+            new_line = True
+        else:
+            new_line = False
