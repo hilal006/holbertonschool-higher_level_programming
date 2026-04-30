9b618ee4c470bfd35fb6b4ea13a88b172c537497
hilal006 <hilalquliyeff@gmail.com>
2026-04-30 16:05:26 +0400

    Add files via upload

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""
+    Function divides all elements of a matrix
+
+    Example:
+       a = 5
+       b = 10
+
+       add_integer(a, b)
+
+       15
+"""
+
+
+def add_integer(a, b=98):
+    """
+        Args:
+            a: first integer
+            b: second integer
+
+        Returns:
+            Sum of the two integers
+    """
+
+    if not isinstance(a, (float, int)):
+        if a is not None:
+            raise TypeError("a must be an integer")
+        raise ValueError("a must be an integer")
+
+    if not isinstance(b, (float, int)):
+        if b is not None:
+            raise TypeError("b must be an integer")
+        raise ValueError("b must be an integer")
+
+    if type(a) is float and type(b) is float:
+        return int(a) + int(b)
+
+    elif type(a) is not float and type(b) is float:
+        return a + int(b)
+
+    elif type(a) is float and type(b) is not float:
+        return int(a) + b
+
+    return a + b
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+    Function divides all elements of a matrix
+
+    Example:
+        matrix = [
+        [1, 2, 3],
+        [4, 5, 6]
+        ]
+        print(matrix_divided(matrix, 3))
+        print(matrix)
+
+        [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+        [[1, 2, 3], [4, 5, 6]]
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        Args:
+            matrix: The matrix list.
+            div: value to divide matrix elements by.
+
+        Returns:
+            New matrix with divided elements.
+    """
+
+    lst = []
+
+    if (not isinstance(matrix, list) or
+            not all(isinstance(row, list) for row in matrix) or
+            not all(isinstance(element, (int, float))
+                    for row in matrix for element in row)):
+        raise TypeError("matrix must be a matrix (list of lists) of"
+                        " integers/floats")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == float('inf'):
+        raise ValueError("[[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]")
+
+    row_length = len(matrix[0])
+    if not all(len(row) == row_length for row in matrix):
+        raise TypeError("Each row of the matrix"
+                        " must have the same size")
+
+    for row in matrix:
+        new_row = []
+        for element in row:
+            c = round(element / div, 2)
+            new_row.append(c)
+        lst.append(new_row)
+
+    return lst
